ba3e1749943db2e2e865af0c9e48f898ae9e681a
ngimdock <ngimdockzemfack@gmail.com>
2023-09-13 20:18:52 +0100

    implement charge message pattern

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
+import { CreateChargeDto } from './dto';
 
 @Injectable()
 export class PaymentsService {
-  getHello(): string {
-    return 'Hello World!!';
+  constructor(private readonly configService: ConfigService) {}
+
+  private readonly stripe = new Stripe(
+    this.configService.get<string>('STRIPE_SECRET_KEY'),
+    { apiVersion: '2023-08-16' },
+  );
+
+  async createCharge({ card, amout }: CreateChargeDto) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card,
+    });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: paymentMethod.id,
+      amount: amout * 100,
+      currency: 'usd',
+      confirm: true,
+      payment_method_types: ['card'],
+    });
+
+    return paymentIntent;
   }
 }
