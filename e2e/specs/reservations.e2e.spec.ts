5b7170a1d3a764ca1f0494e1a11a2f45a903a5f8
ngimdock <ngimdockzemfack@gmail.com>
2023-12-09 17:47:23 +0100

    somes updates

--- e2e/specs/reservations.e2e.spec.ts
+++ e2e/specs/reservations.e2e.spec.ts
@@ -7,34 +7,38 @@ describe('Reservations', () => {
       password: 'Ngimdock1@danmail',
     };
 
-    await createUser(user);
+    // await createUser(user);
 
     const response = await loginUser(user);
 
     const token = await response.json();
 
-    console.log({ token });
+    jwtToken = token.token;
   });
 
   test('Create', async () => {
     expect(true).toBeTruthy();
-    // const reservation = {
-    //   startDate: '12-10-2010',
-    //   endDate: '10-09-2010',
-    //   charge: {
-    //     amout: 44,
-    //     card: {
-    //       cvc: '123',
-    //       exp_month: 10,
-    //       exp_year: 2030,
-    //       number: '4242 4242 4242 4242',
-    //     },
-    //   },
-    // };
+    const reservationDto = {
+      startDate: '12-10-2010',
+      endDate: '10-09-2010',
+      charge: {
+        amout: 44,
+        card: {
+          cvc: '123',
+          exp_month: 10,
+          exp_year: 2030,
+          number: '4242 4242 4242 4242',
+        },
+      },
+    };
 
-    // const response = await createReservation(jwtToken, reservation);
+    const response = await createReservation(jwtToken, reservationDto);
 
-    // const body = await response.json();
+    // expect(response.ok).toBeTruthy();
+
+    const reservation = await response.json();
+
+    console.log({ reservation });
   });
 });
 
@@ -66,7 +70,9 @@ async function loginUser(user: UserDto) {
 }
 
 async function createReservation(jwtToken: string, reservation: any) {
-  const response = await fetch('http:reservations:3000', {
+  console.log({ jwtToken });
+
+  const response = await fetch('http:reservations:3000/reservations', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
